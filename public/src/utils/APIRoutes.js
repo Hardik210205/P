78f9d21ecd860a04b136d131cfda3a7267e327d7
Hardik Parmar <hardikpersonal11@gmail.com>
2025-08-27 15:46:48 +0530

    feat: update API host for production

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://p-wheat-eta.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const SetAvatarRoute = `${host}/api/auth/SetAvatar`;
